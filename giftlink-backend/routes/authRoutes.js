a86bdc809229cebe133e8d62d17fd0a3ea66ed1f
Marcelo <marcelo23.m@gmail.com>
2024-09-01 21:54:39 -0400

    update method

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -5,6 +5,7 @@ const connectToDatabase = require('../models/db');
 const router = express.Router();
 const dotenv = require('dotenv');
 const pino = require('pino');  // Import Pino logger
+const { body, validationResult } = require('express-validator');
 dotenv.config();
 
 const logger = pino();  // Create a Pino logger instance
@@ -94,6 +95,43 @@ router.post('/login', async (req, res) => {
         logger.error(e);
         return res.status(500).json({ error: 'Internal server error', details: e.message });
       }
+});
+
+router.put('/update', async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        logger.error('Validation errors in update request', errors.array());
+        return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        const email = req.headers.email;
+
+        if (!email) {
+            logger.error('Email not found in the request headers');
+            return res.status(400).json({ error: "Email not found in the request headers" });
+        }
+
+        const db = await connectToDatabase();
+        const collection = db.collection("users");
+        
+        const existingUser = await collection.findOne({ email });
+        existingUser.updatedAt = new Date();
+        const updatedUser = await collection.findOneAndUpdate(
+            { email },
+            { $set: existingUser },
+            { returnDocument: 'after' }
+        );
+        const payload = {
+            user: {
+                id: updatedUser._id.toString(),
+            },
+        };
+
+        const authtoken = jwt.sign(payload, JWT_SECRET);
+        res.json({authtoken});
+    } catch (e) {
+        return res.status(500).send('Internal server error');
+    }
 });
 
 module.exports = router;
